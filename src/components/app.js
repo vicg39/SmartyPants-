cd3e2826a242efbf8f1a6fc3ec0e26127287dd06
vicg39 <vagarciamontiel@gmail.com>
2019-05-28 17:50:18 -0400

    app and static files

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,9 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <div>Logo</div>
       </div>
     );
   }
 }
+
