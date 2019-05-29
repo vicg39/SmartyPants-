0d9f08d81a24c50fa6d6db77a58c2774e36e93f1
vicg39 <vagarciamontiel@gmail.com>
2019-05-29 07:59:22 -0400

    setup logo component and styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Logo from "./logo"
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <div>Logo</div>
+        <Logo />
       </div>
     );
   }
---
+++ src/components/logo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class Logo extends Component {
+    render() {
+        return (
+            <div className="logo-main">
+                <img src="/assets/smartypants.png" />
+            </div>
+        )
+    }
+}
+
+export default Logo;
