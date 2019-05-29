b1e753228fe793365c423c91caf839215550e8af
vicg39 <vagarciamontiel@gmail.com>
2019-05-29 12:44:51 -0400

    created post file and added the recent post to it

--- src/components/recent-post.js
+++ src/components/recent-post.js
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 
 import * as actions from "../actions"
 
+import Post from "./post";
+
 class RecentPosts extends Component {
     componentDidMount() {
         this.props.fetchRecentPosts()
@@ -13,9 +15,7 @@ class RecentPosts extends Component {
         const posts = this.props.recentPosts.map((post, index) => {
             if (index < 3) {
                 return (
-                    <li key={index}> 
-                        {post.title}
-                    </li>    
+                    <Post title={post.title}key={index} />
                 )  
             }
               
