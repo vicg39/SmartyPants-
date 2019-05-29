fc066f91fcd736ed29c410a212d3a872a016950f
vicg39 <vagarciamontiel@gmail.com>
2019-05-29 09:36:21 -0400

    created path to results pages

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react"
 import { Field, reduxForm } from "redux-form"
 
+import { withRouter } from "react-router-dom"
+
 class SearchBar extends Component {
 
     handleFormSubmit = function({query}) {
         console.log("handeling submit query", query)
+        this.props.history.push("/results")
     }
 
     renderInput(field) {
@@ -26,5 +29,7 @@ class SearchBar extends Component {
 SearchBar = reduxForm({
     form: "searchBar"
 })(SearchBar)
+
+SearchBar = withRouter(SearchBar)
 
 export default SearchBar;
