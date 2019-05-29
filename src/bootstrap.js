917a370d0a76bb27bcc1525fe1e3e0c09f0f2b72
vicg39 <vagarciamontiel@gmail.com>
2019-05-29 08:44:35 -0400

    new result file and bootstrapimport

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -11,6 +11,8 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 import "./style/main.scss";
 
 import Home from "./components/home"
+import Results from "./components/results"
+
 
 function main() {
   ReactDOM.render(
@@ -18,7 +20,7 @@ function main() {
       <BrowserRouter>
         <Switch>
           <Route path="/" component={Home} />
-          <Route path="/results" component={Home} />
+          <Route path="/results" component={Results} />
         </Switch>
       </BrowserRouter>
     </Provider>,
