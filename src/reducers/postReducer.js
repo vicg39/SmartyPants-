5ce36eb552f01db06c825c4b8bf7a6bf39266675
vicg39 <vagarciamontiel@gmail.com>
2019-05-29 15:10:15 -0400

    got data back with redux and added the SET_RESULTS_POSTS

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,9 +1,10 @@
 import {
-    SET_RECENT_POSTS
+    SET_RECENT_POSTS,
+    SET_RESULTS_POSTS
 } from '../actions/types';
 
 const INIT_STATE = {
-    post: [],
+    resultsPosts: [],
     recentPosts: []
 }
 
@@ -11,7 +12,16 @@ export default function(state = INIT_STATE, action) {
     switch (action.type) {
         case SET_RECENT_POSTS:
             const recentPosts = action.payload
-            return {...state, recentPosts}
+            return {
+                ...state,
+                 recentPosts
+                }
+            case SET_RESULTS_POSTS:
+                const resultsPosts = action.payload;
+                return {
+                    ...state,
+                    resultsPosts
+                }    
             
         default:
             return state;
