d6007dfde29fc167bbfad3965527bd0b44c09f2f
vicg39 <vagarciamontiel@gmail.com>
2019-05-29 08:31:06 -0400

    redux searchbar components and enable typing on searchbar

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react"
+import { Field, reduxForm } from "redux-form"
 
 class SearchBar extends Component {
+
+    handleFormSubmit = function({query}) {
+        console.log("handeling submit query", query)
+    }
+
+    renderInput(field) {
+        return <input type="text" placeholder="SmartyPants" {...field.input} />
+    }
+
     render() {
+        const { handleSubmit } = this.props
         return (
-            <form>
-                <input placeholder="Search SmartyPants" />
+            <form className="search-bar" onSubmit={handleSubmit(
+                this.handleFormSubmit.bind(this))}>
+                <Field name="query" component={this.renderInput} />
             </form> 
             
         )
     }
 }
+
+SearchBar = reduxForm({
+    form: "searchBar"
+})(SearchBar)
 
 export default SearchBar;
