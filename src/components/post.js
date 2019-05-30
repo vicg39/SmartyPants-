7ba7dc9581c70cb1eb497f780b9357ae46ff8577
vicg39 <vagarciamontiel@gmail.com>
2019-05-30 12:33:43 -0400

    styled the links for results

--- src/components/post.js
+++ src/components/post.js
@@ -17,6 +17,26 @@ class Post extends Component {
         return topics;
     }
 
+    getNameForPostLink(str) {
+        var n = str.lastIndexOf('/');
+        var link = str.substring(n + 1, str.length);
+
+        if((n+1) == str.length) {
+            link = str.slice(0, n);
+            n = link.lastIndexOf('/');
+            link = str.substring(n + 1, str.length - 1);
+        }    
+
+        if(link.includes('.html')) {
+            link = link.substring(0, link.length - 5);
+        }
+        if(link.includes('.htm')) {
+            link = link.substring(0, link.length - 4);
+        }
+
+        return link;
+    }
+
     renderLinks() {
         let links = this.props.post_links.map((post_link, index) => {
             return (
@@ -46,19 +66,20 @@ class Post extends Component {
             )
         } else if(this.props.type == 'result') {
             return (
-                <li className="result-post">
+                <li className="result-post"
+                    onMouseEnter={() => this.setState({ height: 70 })}
+                    onMouseLeave={() => this.setState({ height: 0 })}
+                >
                     <div className="result-post__topics">
                         {this.renderTopics()}
                     </div>
                     <div className="result-post__title">
-                        <a href={this.props.url_for_post}
-                            onMouseEnter={() => this.setState({ height: 70 })}
-                            onMouseLeave={() => this.setState({ height: 0 })}
-                        >
-                        {this.props.title}</a>
+                        <a href={this.props.url_for_post}>
+                        {this.props.title}
+                        </a>
                     </div>
                     <Animateheight
-                        duration={2000}
+                        duration={500}
                         height={this.state.height}
                     >
                         <div className="result-post__links">
