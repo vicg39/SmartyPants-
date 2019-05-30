9322fb4390067d9b6a0fda265b2901b582d2f151
vicg39 <vagarciamontiel@gmail.com>
2019-05-30 10:28:15 -0400

    created mixins and organized styles files

--- src/components/results.js
+++ src/components/results.js
@@ -16,7 +16,7 @@ class Results extends Component {
 
     render() {
         return (
-            <div>
+            <div className="results">
                 <Logo size={150}/>
                 <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
                 <ResultsPost />
